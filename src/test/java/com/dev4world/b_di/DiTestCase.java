d424e485b8d919496283579ff16b755a0887c7a4
setvect <setvect@naver.com>
2015-02-10 13:55:28 +0900

    log4j2 로그 적용

--- src/test/java/com/dev4world/b_di/DiTestCase.java
+++ src/test/java/com/dev4world/b_di/DiTestCase.java
@@ -4,30 +4,32 @@ import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DiTestCase {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DiTestCase.class);
 
 	@Test
 	public void DI테스트_Dog() {
-		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"b_di/config_1.xml")) {
+		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("b_di/config_1.xml")) {
 			MyPet pet = (MyPet) context.getBean("myDog");
 			Assert.assertNotNull(pet);
 			String message = pet.getMessage();
-			System.out.println(message);
+			LOGGER.info(message);
 			Assert.assertThat(message, is("주인님"));
 		}
+
 	}
 
 	@Test
 	public void DI테스트_Cat() {
-		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"b_di/config_1.xml")) {
+		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("b_di/config_1.xml")) {
 			MyPet pet = (MyPet) context.getBean("myCat");
 			Assert.assertNotNull(pet);
 			String message = pet.getMessage();
-			System.out.println(message);
+			LOGGER.info(message);
 			Assert.assertThat(message, is("집사 놈아"));
 		}
 	}
