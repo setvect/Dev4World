93c6d926f8d3fe53d020482acebd0d3dbfa20008
setvect <setvect@naver.com>
2015-02-12 20:10:15 +0900

    Spring JUnit

---
+++ src/test/java/com/dev4world/b_di/DiRunWithSpringTestCase.java
@@ -0,0 +1,43 @@
+package com.dev4world.b_di;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:b_di/config_di.xml" })
+public class DiRunWithSpringTestCase {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DiRunWithSpringTestCase.class);
+
+	@Autowired
+	@Qualifier("myDog")
+	private MyPet dogPet;
+
+	@Autowired
+	@Qualifier("myCat")
+	private MyPet catPet;
+
+	@Test
+	public void DI테스트_Dog() {
+		Assert.assertNotNull(dogPet);
+		String message = dogPet.getMessage();
+		LOGGER.info(message);
+		Assert.assertThat(message, is("주인님"));
+	}
+
+	@Test
+	public void DI테스트_Cat() {
+		Assert.assertNotNull(catPet);
+		String message = catPet.getMessage();
+		LOGGER.info(message);
+		Assert.assertThat(message, is("집사 놈아"));
+	}
+}
