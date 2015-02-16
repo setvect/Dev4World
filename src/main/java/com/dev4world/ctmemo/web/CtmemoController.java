36bb42cc65bec615a7cd17d755408c0ad5d74567
luxian <luxian@luxian-VirtualBox>
2015-02-16 22:58:45 +0900

    UI 작업

--- src/main/java/com/dev4world/ctmemo/web/CtmemoController.java
+++ src/main/java/com/dev4world/ctmemo/web/CtmemoController.java
@@ -1,13 +1,21 @@
 package com.dev4world.ctmemo.web;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.servlet.ServletRequest;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.dev4world.ctmemo.CtmemoVo;
 import com.dev4world.ctmemo.service.CtmemoService;
+import com.setvect.common.date.DateUtil;
 
 @Controller
 public class CtmemoController {
@@ -15,8 +23,53 @@ public class CtmemoController {
 	private CtmemoService ctmemoService;
 
 	@RequestMapping("/ctmemoPage.do")
-	public String mainPage(ServletRequest request, ModelMap model) {
+	public String ctmemoPage(ServletRequest request, ModelMap model) {
 		System.out.println(ctmemoService);
 		return "/ctmemoPage/main";
+	}
+
+	@RequestMapping("/listAllCtmemo.json")
+	@ResponseBody
+	public List<CtmemoVo> listAllCtmemo() {
+		List<CtmemoVo> result = new ArrayList<CtmemoVo>();
+		CtmemoVo ctmemoTestData = getCtmemoTestData();
+		CtmemoVo ctmemoTestData1 = getCtmemoTestData2();
+		result.add(ctmemoTestData);
+		result.add(ctmemoTestData1);
+		return result;
+	}
+
+	public static CtmemoVo getCtmemoTestData() {
+		CtmemoVo ctmemo = new CtmemoVo();
+		ctmemo.setContent("내용2\n복슬이");
+		ctmemo.setBgCss("bg_1");
+		ctmemo.setFontCss("font_1");
+		ctmemo.setWidth(130);
+		ctmemo.setHeight(130);
+		ctmemo.setPositionX(220);
+		ctmemo.setPositionY(120);
+		ctmemo.setzIndex(2);
+		Date date = DateUtil.getDateTime("2015-02-13 11:22:11");
+		ctmemo.setRegDate(date);
+		ctmemo.setUptDate(date);
+
+		return ctmemo;
+	}
+
+	public static CtmemoVo getCtmemoTestData2() {
+		CtmemoVo ctmemo = new CtmemoVo();
+		ctmemo.setContent("내용1");
+		ctmemo.setBgCss("bg_1");
+		ctmemo.setFontCss("font_1");
+		ctmemo.setWidth(100);
+		ctmemo.setHeight(100);
+		ctmemo.setPositionX(200);
+		ctmemo.setPositionY(100);
+		ctmemo.setzIndex(1);
+		Date date = DateUtil.getDate("2015-02-14");
+		ctmemo.setRegDate(date);
+		ctmemo.setUptDate(date);
+
+		return ctmemo;
 	}
 }
--- src/main/webapp/WEB-INF/jsp/ctmemoPage/main.jsp
+++ src/main/webapp/WEB-INF/jsp/ctmemoPage/main.jsp
@@ -1,12 +1,51 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
 <title>복슬 메모장</title>
+<script type="text/javascript" src="<c:url value="/js/jquery-1.11.2.js"/>"></script>
+<script type="text/javascript" src="<c:url value="/js/jquery-ui.js"/>"></script>
+<script type="text/javascript" src="<c:url value="/js/util.js"/>"></script>
+<link type="text/css" rel="stylesheet" href="<c:url value="/css/jquery-ui.css"/>" />
+<link type="text/css" rel="stylesheet" href="<c:url value="/css/main.css"/>" />
+<style type="text/css">
+.memo {
+	padding: 0.5em;
+	position: absolute;
+}
+</style>
+
+<script type="text/javascript">
+	var CONTEXT_ROOT = "<%=request.getContextPath()%>";
+	$(function() {
+		loadAllMemo();
+	});
+
+	// 전체 메모장을 불러온다.
+	function loadAllMemo(){
+		$.get(CONTEXT_ROOT + "/listAllCtmemo.json", function(memoList) {
+			$.each(memoList, function() {
+				var item = $("<div id='draggable' class='memo ui-widget-content _item'><span class='_header'></span></div>");
+				item.append(this.content.replace(/\n/g, "<br/>"));
+				item.css("width", this.width)
+						.css("height", this.height)
+						.css("left", this.positionX)
+						.css("top", this.positionY)
+						.css("z-index", this.zIndex);
+				var regDate = new Date(this.regDate);
+				item.find("._header").append(regDate.format("yy.MM.dd HH.mm"));
+				$("#space").append(item);
+			});
+			$("._item" ).draggable();
+		});
+	}
+	
+</script>
 </head>
 <body>
-aaa
+	<div id="space">
+	</div>
 </body>
 </html>
