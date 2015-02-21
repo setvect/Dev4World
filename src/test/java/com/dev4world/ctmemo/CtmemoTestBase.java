235597ae9d8756c34bbabf34d0b243e8e30b386f
luxian <luxian@luxian-VirtualBox>
2015-02-21 14:54:04 +0900

    mybatis 적용 작업중

--- src/test/java/com/dev4world/ctmemo/CtmemoTestBase.java
+++ src/test/java/com/dev4world/ctmemo/CtmemoTestBase.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring/context-*.xml", })
+@ContextConfiguration(locations = { "classpath:spring/context-common.xml", "classpath:spring/context-hibernate.xml" })
 public class CtmemoTestBase {
 
 }
